3673ec564ef4c553110645c22d19b1a691f0d61d
AguileraNico <aguileranicolas1487@gmail.com>
2023-07-03 09:56:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# near-node-contract-connection
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-nvxos8)
---
+++ index.js
@@ -0,0 +1,34 @@
+const nearAPI = require('near-api-js');
+
+const keyPath = './keystore.json';
+
+async function initNear() {
+  const privateKey =
+    'ed25519:yoPhfgH4hJ4V1mDUvVNn8AacHyHX4xavo69nLgY6dWMiaXQDBgMKdbXG7kkt64Jwd9RoJCmG5iPckXbPDJP3EBg';
+  const keyPair = nearAPI.KeyPair.fromString(privateKey);
+  const keyStore = new nearAPI.keyStores.InMemoryKeyStore();
+  keyStore.setKey('testnet', 'nicoknaq.testnet', keyPair);
+  const near = await nearAPI.connect({
+    networkId: 'testnet',
+    nodeUrl: 'https://rpc.testnet.near.org',
+    walletUrl: 'https://wallet.testnet.near.org',
+    helperUrl: 'https://helper.testnet.near.org',
+    explorerUrl: 'https://explorer.testnet.near.org',
+    keyStore,
+  });
+  // console.log(near);
+  const account = await near.account('nicoknaq.testnet');
+  // console.log(account);
+  const contract = new nearAPI.Contract(account, 'nicoknaq.testnet', {
+    viewMethods: ['is_registered'], // Métodos de lectura del contrato
+    changeMethods: ['register_account'], // Métodos de escritura del contrato
+  });
+  contract.register_account({
+    args: {
+      recipient: 'unanueva.testnet',
+      amount: '10000000000000000000000',
+    },
+  });
+}
+
+initNear();
